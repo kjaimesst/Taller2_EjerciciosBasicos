2a716adca4052e2bb13ea2aed25ef5495f783b56
Oriana Jaimes <kamilajaimess6@gmail.com>
2023-03-02 18:41:50 -0600

    se sube el ejercicio 9

--- src/ejercicio9/descuentoTienda.java
+++ src/ejercicio9/descuentoTienda.java
@@ -6,6 +6,16 @@ import java.util.Scanner;
 public class descuentoTienda {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Ofertas");
+        System.out.println("Precio: ");
+        double k;
+        k = sc.nextDouble();
+        double d;
+        d = k*0.15;
+        k = k-d;
+        System.out.println("Total con descuento es:  " + k);
+     
     }
     
 }
